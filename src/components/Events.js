03a92def7cb9b1ae0ef71ea1e2e75fc3b8191c20
Czihi <boslak@student.agh.edu.pl>
2020-08-03 20:19:28 +0200

    Added API

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,34 +2,29 @@ import React from "react";
 import Event from "./Event";
 
 const Events = (props) => {
-    let elements=[];
-    let events=props.events;
+    let elements = [];
+    let events = props.events;
 
-    // List of our friends
-
-// Sort array by date in DESCENDING order
-    if(!props.err) {
+    //sort events by date
+    if (!props.err) {
         events.sort(function (a, b) {
             if (a.datetime > b.datetime) return -1;
             if (a.datetime < b.datetime) return 1;
             return 0;
         });
         events.reverse();
-    console.log(events)
-        for(let event in events){
-            elements.push( <Event key={event}
-                                  mode={event%2} //dark/light mode
-                                  date={events[event].datetime}//API event date
-                                  image={events[event].type} //API event type
-                                  title={events[event].name} //API event title
-                                  desc={events[event].description} //API event description
-                                  url={events[event].registration_url} // API event registration url
+        //fill elements with events
+        for (let event in events) {
+            elements.push(<Event key={event}
+                                 mode={event % 2} //dark/light mode
+                                 date={events[event].datetime}//API event date
+                                 image={events[event].type} //API event type
+                                 title={events[event].name} //API event title
+                                 desc={events[event].description} //API event description
+                                 url={events[event].registration_url} // API event registration url
             />);
         }
     }
-
-
-
 
 
     return (<div>
