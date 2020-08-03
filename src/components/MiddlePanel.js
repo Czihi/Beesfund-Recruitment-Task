ca69bb121684737a0d4c6e461a7379950595dae9
Czihi <boslak@student.agh.edu.pl>
2020-08-03 21:25:15 +0200

    Quick fix for opening event in new window, changed event title to uppercase

--- src/components/MiddlePanel.js
+++ src/components/MiddlePanel.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MiddlePanel = (props) => {
     return (<div className={"middlePanel" + props.mode}>
-        <h3 className="eventTitle">{props.title}</h3>
+        <h3 className="eventTitle">{props.title.toUpperCase()}</h3>
         <div className={"triangle-right" + props.mode}></div>
         <a href={props.url} target="_blank"  rel="noopener noreferrer">
         <button className={"registerButton" + props.mode}>ZAPISZ SIĘ</button>
