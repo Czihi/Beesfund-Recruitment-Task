6c5aece0c615d5a7d5d67adebbede1c5a8a19ec2
Czihi <boslak@student.agh.edu.pl>
2020-08-03 19:19:53 +0200

    Updates

---
+++ precache-manifest.80cb91e6b3644180513b08cf2a7feada.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3410e9404410b873992ec7abcc91e1f",
+    "url": "/Beesfund-Recruitment-Task/static/media/webinar.e3410e94.svg"
+  },
+  {
+    "revision": "5f8e3e8fc6c02675fcb05bc798d7776a",
+    "url": "/Beesfund-Recruitment-Task/static/media/meetup.5f8e3e8f.svg"
+  },
+  {
+    "revision": "aba343238628d651c0eb33da76b81b72",
+    "url": "/Beesfund-Recruitment-Task/static/media/investorsday.aba34323.svg"
+  },
+  {
+    "revision": "19aa7620f6dfa95cd40c",
+    "url": "/Beesfund-Recruitment-Task/static/js/runtime~main.ee0fd4d7.js"
+  },
+  {
+    "revision": "a84f88f2731c5b18b2d4",
+    "url": "/Beesfund-Recruitment-Task/static/js/main.927c4be7.chunk.js"
+  },
+  {
+    "revision": "8fc53bbbbd9a915f83d4",
+    "url": "/Beesfund-Recruitment-Task/static/js/2.7a4e4e3f.chunk.js"
+  },
+  {
+    "revision": "a84f88f2731c5b18b2d4",
+    "url": "/Beesfund-Recruitment-Task/static/css/main.7a9e10a7.chunk.css"
+  },
+  {
+    "revision": "8fc53bbbbd9a915f83d4",
+    "url": "/Beesfund-Recruitment-Task/static/css/2.383b00bf.chunk.css"
+  },
+  {
+    "revision": "75670cccdc4164e93d5a98e5ea2ad7bb",
+    "url": "/Beesfund-Recruitment-Task/index.html"
+  }
+];
